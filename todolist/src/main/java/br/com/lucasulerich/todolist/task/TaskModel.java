9514443e8d3f65824401585cfae2677974937331
Lucas-ulerich <lucas.ulerichsouza@gmail.com>
2024-10-06 07:34:34 -0300

    FEAT: Criando cadastro de tarefas

---
+++ todolist/src/main/java/br/com/lucasulerich/todolist/task/TaskModel.java
@@ -0,0 +1,33 @@
+package br.com.lucasulerich.todolist.task;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Data
+@Entity(name = "tb_tasks")
+public class TaskModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+    private String description;
+
+    @Column(length = 50)
+    private String title;
+    private LocalDate startAt;
+    private LocalDate endAt;
+    private String priority;
+
+    private UUID idUser;
+
+    @CreationTimestamp
+    private LocalDate createdAt;
+
+}
