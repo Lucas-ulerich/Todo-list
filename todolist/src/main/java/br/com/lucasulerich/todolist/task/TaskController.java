f3252a293846485521113b03116172f31a1829b6
Lucas-ulerich <lucas.ulerichsouza@gmail.com>
2024-10-07 20:48:46 -0300

    Feat: Criando mensagens de erros personalizada

--- todolist/src/main/java/br/com/lucasulerich/todolist/task/TaskController.java
+++ todolist/src/main/java/br/com/lucasulerich/todolist/task/TaskController.java
@@ -1,15 +1,16 @@
 package br.com.lucasulerich.todolist.task;
 
+import br.com.lucasulerich.todolist.utils.Utils;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -37,5 +38,31 @@ public class TaskController {
         }
        var task = this.taskRepository.save(taskModel);
        return ResponseEntity.status(HttpStatus.OK).body(task);
+    }
+
+    @GetMapping("/")
+    public List<TaskModel> list(HttpServletRequest request) {
+        var idUser = request.getAttribute("idUser");
+        var task = this.taskRepository.findByIdUser((UUID) idUser);
+        return task;
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
+        var task = this.taskRepository.findById(id).orElse(null);
+
+        if (task == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Tarefa nao encontrada");
+        }
+
+        var idUser = request.getAttribute("idUser");
+
+        if(!task.getIdUser().equals(idUser)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuario nao tem permissao para alterar essa tarefa");
+        }
+        Utils.copyNonNullProperties(taskModel, task);
+
+        var taskUpdated = this.taskRepository.save(task);
+        return ResponseEntity.ok().body(taskUpdated);
     }
 }
--- todolist/src/main/java/br/com/lucasulerich/todolist/task/TaskRepository.java
+++ todolist/src/main/java/br/com/lucasulerich/todolist/task/TaskRepository.java
@@ -2,8 +2,10 @@ package br.com.lucasulerich.todolist.task;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface TaskRepository extends JpaRepository<TaskModel, UUID> {
-
+    List<TaskModel> findByIdUser(UUID idUser);
 }
